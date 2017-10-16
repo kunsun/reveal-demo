f156f65f83ac51caeab1f62cde57e2fbb5f2e25b
yangkun5 <yangkun5@xiaomi.com>
2017-10-16 16:20:27 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,108 @@
+var path = require('path')
+var webpack = require('webpack')
+var { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: {
+    app: './src/main.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    // publicPath: '/dist/',
+    filename: '[name].js',
+    chunkFilename: '[name].chunk.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: require.resolve('reveal'),
+        use: [{
+          loader: 'expose-loader',
+          options: 'Reveal',
+        }]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|woff2|svg|svgz)(\?.+)?$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 3000,
+            name: '[name].[hash].[ext]',
+          }
+        }]
+      },
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map',
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new BundleAnalyzerPlugin(),
+
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: ({ resource }) => (
+        resource &&
+        resource.indexOf('node_modules') >= 0 &&
+        resource.match(/\.js$/)
+      ),
+    }),
+
+    new webpack.optimize.CommonsChunkPlugin({
+      async: 'common-in-lazy',
+      minChunks: ({ resource } = {}) => (
+        resource &&
+        resource.includes('node_modules') &&
+        /axios/.test(resource)
+      ),
+    }),
+
+    new webpack.optimize.CommonsChunkPlugin({
+      async: 'used-twice',
+      minChunks: (module, count) => (
+        count >= 2
+      ),
+    }),
+
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
